6f85e721d650acfa45f09034f14e12a9906962d5
vegaKannur12 <100991705+vegaKannur12@users.noreply.github.com>
2022-05-21 14:29:25 +0530

    uihaw

--- lib/components/customSearchTile.dart
+++ lib/components/customSearchTile.dart
@@ -1,22 +1,22 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class SearchTile {
-//   void searchItem(String enteredKeyword) {
-//     List<Map<String, dynamic>> results = [];
-//     if (enteredKeyword.isEmpty) {
-//       // if the search field is empty or only contains white-space, we'll display all users
-//       results = _allUsers;
-//     } else {
-//       results = _allUsers
-//           .where((product) =>
-//               product["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
-//           .toList();
-//       // we use the toLowerCase() method to make it case-insensitive
-//     }
+class SearchTile {
+  void searchItem(String enteredKeyword, List<Map<String, dynamic>> products) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = products;
+    } else {
+      results = products
+          .where((product) => product["item"]
+              .toLowerCase()
+              .contains(enteredKeyword.toLowerCase()))
+          .toList();
+      // we use the toLowerCase() method to make it case-insensitive
+    }
 
-//     // Refresh the UI
-//     // setState(() {
-//     //   _foundUsers = results;
-//     // });
-//   }
-// }
+    // Refresh the UI
+    // setState(() {
+    //   _foundUsers = results;
+    // });
+  }
+}
--- lib/screen/itemSelection.dart
+++ lib/screen/itemSelection.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:orderapp/components/commoncolor.dart';
+import 'package:orderapp/components/customSearchTile.dart';
 import 'package:provider/provider.dart';
 
 import '../controller/controller.dart';
@@ -14,7 +15,7 @@ class ItemSelection extends StatefulWidget {
 class _ItemSelectionState extends State<ItemSelection> {
   List<Map<String, dynamic>> products = [];
   int? selected;
-
+  SearchTile search = SearchTile();
   @override
   void initState() {
     // TODO: implement initState
@@ -42,75 +43,73 @@ class _ItemSelectionState extends State<ItemSelection> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          // Container(
-          //   height: size.height * 0.04,
-          //   decoration: BoxDecoration(
-          //     color: Colors.indigo,
-          //     borderRadius: BorderRadius.only(
-          //       // bottomLeft: Radius.circular(50),
-          //       // bottomRight: Radius.circular(50),
-          //     ),
-          //   ),
-          //   // child: Text("Count"),
-          // ),
-          Container(
-            width: size.width * 0.95,
-            height: size.height * 0.1,
-            child: TextField(
-              // onChanged: (value) => _runFilter(value),
-              decoration: const InputDecoration(
-                  labelText: 'Search', suffixIcon: Icon(Icons.search)),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: products.length,
-                itemBuilder: (BuildContext context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(left: 0.4, right: 0.4),
-                    child: ListTile(
-                      title: Text(
-                        '${products[index]["code"]}' + '-'  + '${products[index]["item"]}',
-                        style:
-                            TextStyle(color: Colors.green[800], fontSize: 18),
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Container(
-                              width: size.width * 0.09,
-                              child: TextFormField(
-                                keyboardType: TextInputType.number,
-                                decoration: InputDecoration(
-                                    border: InputBorder.none, hintText: "qty"),
-                              )),
-                          SizedBox(
-                            width: 10,
+      body: Consumer<Controller>(
+        builder: (context, value, child) {
+          return Column(
+            children: [
+              Container(
+                width: size.width * 0.95,
+                height: size.height * 0.1,
+                child: TextField(
+                  onChanged: (value) => Provider.of<Controller>(context, listen: false).searchProcess( value),
+                  decoration: const InputDecoration(
+                      labelText: 'Search', suffixIcon: Icon(Icons.search)),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: products.length,
+                    itemBuilder: (BuildContext context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 0.4, right: 0.4),
+                        child: ListTile(
+                          title: Text(
+                            '${products[index]["code"]}' +
+                                '-' +
+                                '${products[index]["item"]}',
+                            style: TextStyle(
+                                color: Colors.green[800], fontSize: 18),
                           ),
-                          IconButton(
-                            icon: Icon(Icons.add),
-                            onPressed: () {
-                              setState(() {
-                                selected=index;
-                              });
-                            },
-                            color: selected== index ? P_Settings.addbutonColor:Colors.black,
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                  width: size.width * 0.09,
+                                  child: TextFormField(
+                                    keyboardType: TextInputType.number,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: "qty"),
+                                  )),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.add),
+                                onPressed: () {
+                                  setState(() {
+                                    selected = index;
+                                  });
+                                },
+                                color: selected == index
+                                    ? P_Settings.addbutonColor
+                                    : Colors.black,
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {},
+                                color: Theme.of(context).errorColor,
+                              )
+                            ],
                           ),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {},
-                            color: Theme.of(context).errorColor,
-                          )
-                        ],
-                      ),
-                    ),
-                  );
-                }),
-          ),
-        ],
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          );
+        },
       ),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(left: 100, right: 150),
